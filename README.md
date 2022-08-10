77f37c09630b8821c65ab5d349b314740fbd30db
lijinhong11 <mcjrhugh@163.com>
2022-08-10 19:05:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# BossShopPro
+
+BossShopPro is one of the most player-friendly and multifunctional GUI plugins ever! Say goodbye to all the annoying command- or signshops!
+It can not only be used to create shops but for every kind of menu. Here are just a few examples:
+* Shop (Buy or sell items)
+* Kits
+* A menu that allows players to execute commands with a simple click
+* Server selector (when having multiple servers connected)
+* Warp menu
+
+## About This Project
+This project is an improved version of bosshoppro.This plugin only supports versions 1.16 and above.And fixed some bugs of the original project, see "Changes to the original project".
+
+## Building BossShopPro
+
+A few notes regarding building BSP: the current state of the code, unfortunately, requires you to manually add (the jars of) a few other plugins to the project in order to be able to successfully build BSP. As BSP includes support for many different plugins, such as Kingdoms and EpicSpawners, you need to add those to the project (at least those, which I was not able to add directly via Maven). It requires some initial effort to download those plugins and add them to the project.
+
+## Changes to the original project
+* Fixed the problem of putting minecraft color together when looking for hex color.  
+* Remove VotingPlugin, LilyPad, CommandPoints, Kingdoms, EpicSpawners support.  
+* Update dependency and change some prompt. 
+* Something may be added in the future.
+* The plugin usage remains unchanged.
+
+## API
+Information regarding the BossShopPro API can be found here: [Link](https://www.spigotmc.org/wiki/bossshoppro-api/).
---
+++ src/main/java/org/black_ixx/bossshop/core/rewards/BSRewardTypeTeleport.java
@@ -0,0 +1,73 @@
+package org.black_ixx.bossshop.core.rewards;
+
+import org.black_ixx.bossshop.core.BSBuy;
+import org.black_ixx.bossshop.managers.ClassManager;
+import org.black_ixx.bossshop.managers.misc.InputReader;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+
+public class BSRewardTypeTeleport extends BSRewardType{
+    @Override
+    public Object createObject(Object o, boolean force_final_state) {
+        return InputReader.readString(o, true);
+    }
+
+    @Override
+    public boolean validityCheck(String item_name, Object o) {
+        boolean b = false;
+        if (o != null) {
+            String[] objs = ((String)o).trim().split("[|]");
+            if(objs.length == 4 || objs.length == 6){
+                b = true;
+            }else{
+                ClassManager.manager.getBugFinder().severe("Was not able to create ShopItem " + item_name + "! The reward object needs to be written in the format of 'WorldName|X axis|Y axis|Z axis' or 'WorldName|X axis|Y axis|Z axis|yaw|pitch'.");
+            }
+        }else {
+            ClassManager.manager.getBugFinder().severe("Was not able to create ShopItem " + item_name + "! The reward object needs to be written in the format of 'WorldName|X axis|Y axis|Z axis' or 'WorldName|X axis|Y axis|Z axis|yaw|pitch'.");
+        }
+        return b;
+    }
+
+    @Override
+    public void enableType() {
+    }
+
+    @Override
+    public boolean canBuy(Player p, BSBuy buy, boolean message_if_no_success, Object reward, ClickType clickType) {
+        return true;
+    }
+
+    @Override
+    public void giveReward(Player p, BSBuy buy, Object reward, ClickType clickType) {
+        String[] rewards = ((String)reward).trim().split("[|]");
+        World w = Bukkit.getWorld(rewards[0]);
+        Location loc;
+        if(rewards.length == 6){
+            loc = new Location(w, Double.parseDouble(rewards[1]), Double.parseDouble(rewards[2]), Double.parseDouble(rewards[3]),
+                    Float.parseFloat(rewards[4]), Float.parseFloat(rewards[5]));
+        }else {
+            loc = new Location(w, Double.parseDouble(rewards[1]), Double.parseDouble(rewards[2]), Double.parseDouble(rewards[3]));
+        }
+        p.teleport(loc);
+    }
+
+    @Override
+    public String getDisplayReward(Player p, BSBuy buy, Object reward, ClickType clickType) {
+        String[] rewards = ((String)reward).trim().split("[|]");
+        return ClassManager.manager.getMessageHandler().get("Display.Teleport").replace("%world%",rewards[0])
+                .replace("%x%",rewards[1]).replace("%y%",rewards[2]).replace("%z%",rewards[3]);
+    }
+
+    @Override
+    public String[] createNames() {
+        return new String[]{"Teleport","teleport","tp"};
+    }
+
+    @Override
+    public boolean mightNeedShopUpdate() {
+        return false;
+    }
+}
